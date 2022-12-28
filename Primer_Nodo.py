c9ea7f8fb01e31d248bc88b99e2d9a1ba176b698
Natalia Arenas <71849061+nat1999@users.noreply.github.com>
2022-12-28 17:32:16 -0500

    Create Primer_Nodo.py

---
+++ Primer_Nodo.py
@@ -0,0 +1,30 @@
+# Nodo_Ros
+
+#!/usr/bin/env python3
+# encoding: utf-8
+
+#Linea 1 - “Shebang”,le indicamos a la máquina con qué programa lo vamos a ejecutar.
+#Linea 2 - Python 3 - asume que solo se utiliza ASCII en el código fuente
+# para usar utf-8 hay que indicarlo al principio de nuestro script : encoding: utf-8
+
+#Importar la API de Ros para python
+import rospy
+
+def nodo():  # Definimos una función nodo
+
+    #Inicializamos nuestro nodo y le asignamos un nombre = nodo1
+    rospy.init_node('nodo1')
+
+    # Declaramos una variable mensaje y asignamos una cadena de caracteres
+    mensaje = "Hola Mundo"
+
+    #Imprime en consola el valor de la variable mensaje
+    rospy.loginfo(mensaje)
+
+
+if __name__ == '__main__':
+    try:
+        nodo()                          # Llamamos a la función nodo
+    except rospy.ROSInterruptException: # Cuando hay una excepción 
+        pass
+        
